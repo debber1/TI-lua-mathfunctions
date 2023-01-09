76bc720d296514e2fa92ebc90be917bd867d0ed9
debber1 <robbe.decapmaker@hotmail.be>
2023-01-09 15:35:46 +0100

    Calculate and output eigenvalues of a 3x3

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ local w = screen:width();
 var.store("test",1);
 var.store("UnitMatrix", {{1,0,0},{0,1,0},{0,0,1}});
 var.store("SystemMatrix", {{1,0,0},{0,1,0},{0,0,1}});
+var.store("ZeroMatrix",{{0}, {0} ,{0}});
 
 -- This is the entry of our program. This executes when the document gets loaded in the calculator 
 function on.paint(gc)
@@ -14,7 +15,6 @@ function on.paint(gc)
 	counter = var.recall("test");
 	gc:drawString(tostring(counter),0,20);
 	var.store("test", counter+1);
-	var.monitor("test");
 
 	-- Now we print the eigenvalues of the SystemMatrix
 	gc:drawString("The eigenvalues are:", 0, 40);
@@ -24,6 +24,33 @@ function on.paint(gc)
 	for index, value in pairs(result) do
 		gc:drawString("Eigenvalue "..tostring(index).." is "..tostring(value), 10, 60 +20*loopcount);
 		loopcount = loopcount +1;
+	end
+
+	-- Make a list with unique eigenvalues
+	local hash = {};
+	local unique = {};
+	local found = false;
+	for _, v in pairs(result) do
+		for index, value in pairs(unique) do
+			if value == v then
+				found = true;	
+			end
+		end
+		if found == false then
+			unique[#unique+1] = v;
+		end
+		found = false;
+	end
+
+	-- Now we print the eigenvector of the SystemMatrix
+	gc:drawString(tostring(#unique), 60,20);
+	loopcount = 0;
+	gc:drawString("The eigenvectors are:", 0, 120);
+	for index, eigenvalue in pairs(unique) do
+		gc:drawString(tostring(eigenvalue)..":",10, 140+20*loopcount);
+		local vector, error = calculateEigenVector("SystemMatrix", eigenvalue, "UnitMatrix", "ZeroMatrix");
+		gc:drawString(tostring(vector), 40, 140+20*loopcount);
+		loopcount = loopcount + 1
 	end
 end
 
@@ -35,4 +62,9 @@ end
 -- This function returns the eigenvalues of a matrix stored in calculator memory
 function calculateEigenValues(matrix)
 	return math.eval("eigVl("..matrix..")");
+end
+
+-- This function calculates the eigenvector of a given matrix for a given eigenvalue
+function calculateEigenVector(matrix, eigenvalue, unitmatrix, zerovector)
+	return math.evalStr("solve(("..matrix.."-"..tostring(eigenvalue)..unitmatrix..")[[x][y][z]]="..zerovector..",x,y,z)");
 end
